b863c66b7a65f024d3323dad8eb9da16efb96147
AyshanGambarova <ayshanag@code.edu.az>
2022-11-30 11:34:22 +0400

    infinite scroll added

--- src/views/Products/index.vue
+++ src/views/Products/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <h1>Products</h1>
-    <v-row class="mb-6" no-gutters>
+    <v-row class="mb-6" no-gutters style="height: 500px; overflow-y: scroll" @scroll="getProducts">
       <v-col md="3" v-for="product in products" :key="product.id">
         <v-card :loading="loading" class="mx-auto my-12" max-width="374">
           <v-img height="250" :src="product.images[0]"></v-img>
--- src/views/Products/main.ts
+++ src/views/Products/main.ts
@@ -7,30 +7,59 @@ export default defineComponent({
   setup() {
     let products = ref<any>([])
     let loading = ref<boolean>(false)
-    async function getProducts() {
+    async function getProducts(event: any) {
+      try {
+        let {scrollTop, clientHeight, scrollHeight} = event.target
+        if (!loading.value && scrollTop + clientHeight >= (scrollHeight * 4) / 5) {
+          loading.value = true
+          const response = await axios({
+            method: 'GET',
+            url: `https://dummyjson.com/products`,
+            params: {
+              skip: 0,
+              limit: 10
+            }
+          })
+          loading.value = false
+          products.value = [...products.value, ...response.data.products]
+          console.log(response)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    async function getProductsInit() {
       try {
         const response = await axios({
           method: 'GET',
-          url: `https://dummyjson.com/products`
+          url: `https://dummyjson.com/products`,
+          params: {
+            skip: 0,
+            limit: 10
+          }
         })
-        products.value=response.data.products;
+
+        products.value = response.data.products
         console.log(response)
       } catch (error) {
         console.log(error)
       }
     }
+
     function reserve() {
-    loading.value=true
-    setTimeout(() => (loading.value = false), 2000)
+      loading.value = true
+      setTimeout(() => (loading.value = false), 2000)
     }
-    
+
     onMounted(() => {
-      getProducts()
+      getProductsInit()
     })
     return {
       products,
       reserve,
       loading,
+      getProducts
     }
   }
 })
